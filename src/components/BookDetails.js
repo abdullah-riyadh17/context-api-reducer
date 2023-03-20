0df61b6d6d38d18158bc92a8708e0cce08138149
Abdullah Riyadh <abdullahriyadhcse@gmail.com>
2023-03-20 15:02:23 +0600

    book list and details added

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { BookContext } from "../contexts/BookContext";
 
-const BookDetails = () => {
-  return <div></div>;
+const BookDetails = ({ book }) => {
+  const { dispatch } = useContext(BookContext);
+  return (
+    <li onClick={() => dispatch({ type: "REMOVE_BOOK", id: book.id })}>
+      <div className="title"></div>
+      <div className="author"></div>
+    </li>
+  );
 };
 
 export default BookDetails;
--- src/components/BookLists.js
+++ src/components/BookLists.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { BookContext } from "../contexts/BookContext";
+import BookDetails from "./BookDetails";
 
 const BookLists = () => {
-  return <div></div>;
+  const { books } = useContext(BookContext);
+
+  return books.length ? (
+    <div className="book-list">
+      <ul>
+        {books.map((book) => {
+          return <BookDetails book={book} key={book.id}></BookDetails>;
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div className="empty">There is no books to read ...</div>
+  );
 };
 
 export default BookLists;

c6024141b8bd70debe11284da2b89da08181f24f
Abdullah Riyadh <abdullahriyadhcse@gmail.com>
2023-03-20 13:51:41 +0600

    Book Form added

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { BookContext } from "../contexts/BookContext";
 
 const NewBookForm = () => {
-  return <div></div>;
+  const { dispatch } = useContext(BookContext);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({ type: "ADD_BOOK", book: { title, author } });
+    setTitle("");
+    setAuthor("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Book Title"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Author Name"
+        value={author}
+        onChange={(event) => setAuthor(event.target.value)}
+        required
+      />
+      <button type="submit">Add book</button>
+    </form>
+  );
 };
 
 export default NewBookForm;

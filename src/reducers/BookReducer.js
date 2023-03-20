682eb199e19fe0aeab3ef698ee0e04724b792a40
Abdullah Riyadh <abdullahriyadhcse@gmail.com>
2023-03-20 15:49:47 +0600

    reducer added

--- src/reducers/BookReducer.js
+++ src/reducers/BookReducer.js
@@ -1,7 +1,21 @@
-import React from "react";
+import { uuid } from "uuidv4";
 
-const bookReducer = () => {
-  return <div></div>;
+export const bookReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [
+        ...state,
+        {
+          title: action.book.title,
+          author: action.book.author,
+          id: uuid(),
+        },
+      ];
+    case "REMOVE_BOOK":
+      return state.filter((book) => book.id !== action.id);
+    default:
+      return state;
+  }
 };
 
 export default bookReducer;
